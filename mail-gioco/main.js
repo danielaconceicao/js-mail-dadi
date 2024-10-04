17e65f6b022798f5ad76a00d4b8aca4d97262411
danielaconceicao <daniela.3597@gmail.com>
2024-10-04 10:07:50 +0200

    correcting the exercise

--- mail-gioco/main.js
+++ mail-gioco/main.js
@@ -1,9 +1,22 @@
 let email = prompt('inserisci la tua email');
-
 let emailControl = ['user123@gmail.com', 'user456@gmail.com', 'user789@gmail.com'];
+let canAccess;
 
-if (emailControl.includes(email)) {
+canAccess = emailControl.includes(email)
+if (canAccess) {
     console.log(`Bentornato ${email}`);
 } else {
     console.log(`Mi dispiace ${email} non é consentito il tuo accesso`);
 }
+
+/* 
+for (let i = 0; i < emailControl.length; i++){
+thisEmailControl = emailControl;
+
+    if (thisEmailControl === email) {
+    canAccess = true;
+    break;
+    }
+}
+console.log(canAccess);
+*/
